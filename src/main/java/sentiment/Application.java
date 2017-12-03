3d3957c74331722e928511bb83697795590c5608
schne5 <elisa@ElisasPc.bfh.ch>
2017-12-03 10:22:52 +0100

    lexicon

--- src/main/java/sentiment/Application.java
+++ src/main/java/sentiment/Application.java
@@ -1,7 +1,16 @@
 package sentiment;
 
+import features.SentimentWordCountResult;
+import features.SentimentWordCounter;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Application {
     public static void main(String[] args){
-
+        Path path = Paths.get("C:\\Users\\elisa\\IdeaProjects\\sentimentanalyse.parent\\goldstandard\\pos", "cv000_29590.txt");
+        SentimentWordCountResult result = new SentimentWordCounter().countSentimentWords(path);
+        System.out.println("Negative: "+result.getNegativeWordCount());
+        System.out.println("Positive: "+result.getPositiveWordCount());
     }
 }
