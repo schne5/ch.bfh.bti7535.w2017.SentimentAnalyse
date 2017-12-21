1f1bafaf52972bce19e7324ae4da9135cf718d59
Anna <schea2@bfh.ch>
2017-12-21 18:22:45 +0100

    remove stop words from String

--- src/main/java/features/TextPreProcessor.java
+++ src/main/java/features/TextPreProcessor.java
@@ -1,21 +1,20 @@
 package features;
 
-import java.util.List;
-
 public class TextPreProcessor {
-    public static String removeStopWords(String text){
-        return StopWordElimination.removeStopWordsFromText(text);
-    }
-    public static String increaseWordWeight(int factor, double textSection,String text){
-        int length = text.length();
-        int startIndex = (int)((1-textSection) *length);
-        while(text.charAt(startIndex) != '.' && text.charAt(startIndex)!=','
-                && text.charAt(startIndex)!= '?' && text.charAt(startIndex)!= '!' && text.charAt(startIndex)!= ' '){
-            startIndex++;
-        }
-        String section = text.substring(startIndex);
-        for(int i=0; i<factor-1;i++)
-            text += section;
-       return text+" "+section;
-    }
+	public static String removeStopWords(String text) {
+		return StopWordElimination.removeStopWords(text);
+	}
+
+	public static String increaseWordWeight(int factor, double textSection, String text) {
+		int length = text.length();
+		int startIndex = (int) ((1 - textSection) * length);
+		while (text.charAt(startIndex) != '.' && text.charAt(startIndex) != ',' && text.charAt(startIndex) != '?'
+				&& text.charAt(startIndex) != '!' && text.charAt(startIndex) != ' ') {
+			startIndex++;
+		}
+		String section = text.substring(startIndex);
+		for (int i = 0; i < factor - 1; i++)
+			text += section;
+		return text + " " + section;
+	}
 }
